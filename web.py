f60cdfb9632a1a60de1f2e1d9610c8fd23711d55
EED <EED@CORP.AMAZONWORKSPACES.COM>
2019-02-07 01:06:18 +0100

    webscrapping cat 2

--- web.py
+++ web.py
@@ -54,12 +54,17 @@ for d in pkg_resources.working_set:
     #Excel output
 l = [] #temporary list 
 d = {} #temporary dictionary
-df1='' #Pandas Dataframes
+l_df = [] #list of Pandas Dataframes
+for I in range(3):
+    df_tmp = DataFrame([])
+    l_df.append(df_tmp)
+
+
 sheet_out = "Tabelle1"
     # Steuervariablen
 max_web_i = [float('Inf') #cat0 -> is not used
             ,float('Inf') #cat1
-            ,2 #cat2
+            ,float('Inf') #cat2
             ]  #maximum loop for webscrapping 
 # max_web_i = float('Inf') # scrap all
 
@@ -67,9 +72,9 @@ max_web_i = [float('Inf') #cat0 -> is not used
 url = url_root+'/produkte'
     #Control
 cr = 0 #Count of Web requests
-
 load_df = [True #cat0
-        ,False #cat1
+        ,True #cat1
+        ,False #cat2
         ]
 logging.info('load_df = ' + str(load_df))
 I=0;J=0
@@ -78,13 +83,19 @@ I=0;J=0
 #-------------------------------------------------
 logging.info('\n' + '-'*30 + '\n1) START\n' + '-'*30 + '\n')
 
-
-logging.info('\n' + '-'*30 + '\n1.1) Cat0\n' + '-'*30 + '\n')
+#-------------------------------------------------
+#1.1) Cat0
+#-------------------------------------------------
+level = 0
+logging.info('\n' + '-'*30 + '\n1.' +str(level) + ') Cat1\n' + '-'*30 + '\n')
 cat_label = 'Kategorie_0'
 file = r'out' + '\\' + cat_label
 
-if load_df[0]:
-    df1 = pd.read_pickle(file + '.pkl')
+if load_df[level]:
+    df = pd.read_pickle(file + '.pkl')
+    logging.info('df was loaded from disk')
+    logging.debug(df.head())
+    l_df[level] = df
 else:
     if TEST:
         url = r'\in\000_view-source_https_produkte.html'
@@ -118,19 +129,26 @@ else:
                 d["Kategorie - Level1"] = ""
                 d["Kategorie - Level2"] = ""
                 l.append(d)
-    df1 = DataFrame(l)
-    logging.info(df1.head())
-    df1.to_csv( file + '.csv') 
-    df1.to_pickle(file + '.pkl')
+    df = DataFrame(l)
+    logging.info(df.head())
+    df.to_csv( file + '.csv') 
+    df.to_pickle(file + '.pkl')
+    l_df[level] = df
 
-
-logging.info('\n' + '-'*30 + '\n1.2) Cat1\n' + '-'*30 + '\n')
-
-cat_label = 'Kategorie_1'
+#-------------------------------------------------
+#1.2) Cat1
+#-------------------------------------------------
+level = 1
+logging.info('\n' + '-'*30 + '\n1.' +str(level) + ') Cat1\n' + '-'*30 + '\n')
+df1 = l_df[level-1]
+cat_label = 'Kategorie_{}'.format(level)
 file = r'out' + '\\' + cat_label
 
-if load_df[1]:
-    df2 = pd.read_pickle(file + '.pkl')
+if load_df[level]:
+    df = pd.read_pickle(file + '.pkl')
+    logging.info('df was loaded from disk')
+    logging.debug(df.head())
+    l_df[level] = df
 else:
     I = 0;l=[]
     for index,row in df1.iterrows():
@@ -141,8 +159,6 @@ else:
         soup = BeautifulSoup(c,"html.parser")
         all_cat1 = soup.find("aside",{"class":"sidemenu categories syncheight"})
         sublinks = all_cat1.find_all('li',class_="")
-        len(sublinks)
-        sublinks[0].find('a')
         for link in sublinks:
             link_a = link.find('a')
             has_link = link_a.has_attr('href')
@@ -159,13 +175,87 @@ else:
                     d["Kategorie - Level1"] = link_a.text
                     d["Kategorie - Level2"] = ""
                     l.append(d)
-        if index >= max_web_i[1]-1:
+        if index >= max_web_i[level]-1:
             logging.warning('Maximum number of webscapping : {}'.format(max_web_i))
             break
-    df2 = DataFrame(l)
-    logging.info(df1.head())
-    df2.to_csv( file + '.csv') 
-    df2.to_pickle(file + '.pkl')
+    df = DataFrame(l)
+    logging.info(df.head())
+    df.to_csv( file + '.csv') 
+    df.to_pickle(file + '.pkl')
+    l_df[level] = df
+
+#-------------------------------------------------
+#1.2) Cat2
+#-------------------------------------------------
+level = 2
+logging.info('\n' + '-'*30 + '\n1.' +str(level) + ') Cat1\n' + '-'*30 + '\n')
+df1 = l_df[level-1]
+
+t2 = time.clock()
+cat_label = 'Kategorie_{}'.format(level)
+file = r'out' + '\\' + cat_label
+
+if load_df[level]:
+    df = pd.read_pickle(file + '.pkl')
+    logging.info('df was loaded from disk')
+    logging.debug(df.head())
+    l_df[level] = df
+else:
+    I = 0;l=[]
+    for index,row in df1.iterrows():
+        url = row["url"]
+        r = requests.get(url);cr = cr+1
+        logging.debug('REQUEST No ' + str(cr) + ': ' + url )
+        c = r.content
+        soup = BeautifulSoup(c,"html.parser")
+        #get active subcat
+        
+        active_subcat = soup.find('span',{'class':'subcategory-activ'})
+        active_subcat_text = "" if active_subcat is None else active_subcat.text
+        if active_subcat_text == row["Kategorie - Level1"]:
+            d={};I+=1
+            logging.debug(row["Kategorie - Level1"] + ' has no subcats')
+            d["url"] = row["url"]
+            d["Kategorie - Level0"] = row["Kategorie - Level0"]
+            d["Kategorie - Level1"] = row["Kategorie - Level1"]
+            d["Kategorie - Level2"] = row["Kategorie - Level1"]
+            #d["index"] = I
+            l.append(d)
+        else:
+            all_cat1 = soup.find("aside",{"class":"sidemenu categories syncheight"})
+            sublinks = all_cat1.find_all('li',class_="")
+            for link in sublinks:
+                link_a = link.find('a')
+                has_link = link_a.has_attr('href')
+                if has_link:
+                    d={}; I+=1
+                    logging.debug('I=' + str(I) + ' - ' + link_a.text + ' - ' + link_a['href'])
+                    d["index"] = I
+                    if TEST:
+                        d["url"] = link_a['href'] #put absolute URL in offline file 
+                    else:
+                        d["url"] = url_root + link_a['href']
+                        d["Kategorie - Level0"] = row["Kategorie - Level0"]
+                        d["Kategorie - Level1"] = row["Kategorie - Level1"]
+                        d["Kategorie - Level2"] = link_a.text
+                        l.append(d)
+        if index >= max_web_i[level]-1:
+            logging.warning('Maximum number of webscapping : {}'.format(max_web_i))
+            break
+    df = DataFrame(l)
+    logging.info(df.head())
+    df.to_csv( file + '.csv') 
+    df.to_pickle(file + '.pkl')
+    l_df[level] = df
+
+
+
+T2 = time.clock()
+RUNTIME2 = time.strftime("%H:%M:%S", time.gmtime(T2-t2))
+logging.info('\n\n RUNTIME - Level {}: '.format(level) + RUNTIME2)
+
+
+
 
 T = time.clock()
 RUNTIME = time.strftime("%H:%M:%S", time.gmtime(T-t0))
